a6b192fa5c2f487f1a53cc78a5823b4589f6e144
PeterG-ithub <98351061+PeterG-ithub@users.noreply.github.com>
2023-06-21 18:30:43 -0700

    add mainControl and generateNumber function
    mainControl function is the controller of
    the calculator. Depending on what the user
    clicks, the input will go to the mainControl
    to decide which function to run.
    
    generateNumber function generates a number
    in the screen.

--- javascript.js
+++ javascript.js
@@ -4,7 +4,50 @@ const buttons = document.querySelectorAll('btn');
 
 buttons.forEach(btn => {
     btn.addEventListener('click', event => {
-        screenText.textContent = event.target.id;
+        mainControl(event.target.id);
         console.log(event.target.id);
     });
 });
+
+function mainControl(btnInput) {
+    switch(btnInput) {
+        case 'one':
+            generateNumber(1);
+            break;
+        case 'two':
+            generateNumber(2);
+            break;
+        case 'three':
+            generateNumber(3);
+            break;
+        case 'four':
+            generateNumber(4);
+            break;
+        case 'five':
+            generateNumber(5);
+            break;
+        case 'six':
+            generateNumber(6);
+            break;
+        case 'seven':
+            generateNumber(7);
+            break;
+        case 'eight':
+            generateNumber(8);
+            break;
+        case 'nine':
+            generateNumber(9);
+            break;
+        case 'zero':
+            generateNumber(0);
+            break;
+    }
+}
+
+function generateNumber(number) {
+    if(screenText.textContent == '0') {
+        screenText.textContent = number;
+    } else {
+        screenText.textContent += number;
+    }
+}
